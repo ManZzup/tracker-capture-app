beadc48d5dc53e7352a164f6b94bf7cb38e08e25
manzzup <manujith.nc@gmail.com>
2020-03-19 16:02:12 +0530

    fixed webpack config issue

--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,7 @@ module.exports = {
             ]),
             scripts: makeScriptTags([
                 'core/tracker-capture.js',
-                '../main.js',
+                'vendor/main/main.js',
             ]),
         }),
     ],
